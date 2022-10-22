90fe00609129e020c4fa1ef0f09b54005141266a
craftrac <dev@orbitplum.com>
2022-10-22 10:48:42 +0300

    convert to class

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ import nstatic from 'node-static';
 
 const PORT = 3000;
 const STATIC_DIR = 'static';
-const __dirname = process.cwd() + '/' + STATIC_DIR;
-var file = new(nstatic.Server)(__dirname);
 
-http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(PORT);
+export default class ClientJsTest {
+  constructor(staticPath) {
+    const __dirname = process.cwd() + '/' + staticPath || process.cwd() + '/' + STATIC_DIR;
+    const file = new(nstatic.Server)(__dirname);
+    http.createServer(function (req, res) {
+      file.serve(req, res);
+    }).listen(PORT);
+  }
+}
+
+
