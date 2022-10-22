5be140c327feacec003686a357e75b6bdbeeac58
craftrac <dev@orbitplum.com>
2022-10-22 11:17:23 +0300

    fixes, separate conditional standalone server or module use

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ import nstatic from 'node-static';
 const PORT = 3000;
 const STATIC_DIR = 'static';
 
-export default class ClientJsTest {
-  constructor(staticPath) {
-    const __dirname = process.cwd() + '/' + staticPath || process.cwd() + '/' + STATIC_DIR;
-    const file = new(nstatic.Server)(__dirname);
+class ClientJsTest {
+  static serve(staticPath) {
+    if (typeof staticPath != 'undefined') {
+      // use tester as a module
+      console.log('running as a module');
+      this.dirname = process.cwd() + '/' + staticPath;
+    }
+    else {
+      // use tester standalone
+      console.log('running standalone server');
+      this.dirname = process.cwd() + '/' + STATIC_DIR;
+    }
+    const file = new(nstatic.Server)(this.dirname);
     http.createServer(function (req, res) {
       file.serve(req, res);
     }).listen(PORT);
   }
 }
 
+ClientJsTest.serve();
 
+export default ClientJsTest;
